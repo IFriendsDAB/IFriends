b352b98170daddc8120f3c8552522a309b346916
Amir Ince <84426843+amirince@users.noreply.github.com>
2024-04-09 16:29:01 -0400

    IOS Project

---
+++ IFriends/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IFriends
+//
+//  Created by Amir Ince on 4/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
